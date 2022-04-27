f8c0666dc80810490deda18c06eb31931341fda6
Yorleny <yorleny.a.v@gmail.com>
2022-04-27 14:45:21 -0600

    Diseño

--- ControlAsistenciaFrontEnd/FrontEnd/Startup.cs
+++ ControlAsistenciaFrontEnd/FrontEnd/Startup.cs
@@ -33,6 +33,7 @@ namespace FrontEnd
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            
             services.AddControllersWithViews();
             services.AddRazorPages();
 
